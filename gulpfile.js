f58f607cdfaec2fe9be144575d264bb6a0204976
Joaofre <joaovitor6586@gmail.com>
2023-02-27 18:54:33 -0300

    Added BrowserSync and Gulp Watch

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,4 @@
+const { series, parallel} = require('gulp')
 const gulp = require('gulp')
 const concat = require('gulp-concat')
 const cssmin = require('gulp-cssmin')
@@ -6,10 +7,14 @@ const uglify = require('gulp-uglify')
 const image = require('gulp-imagemin')
 const stripJs = require('gulp-strip-comments')
 const stripCss = require('gulp-strip-css-comments')
+const htmlmin = require('gulp-htmlmin');
+const babel = require('gulp-babel')
+const browserSync = require('browser-sync').create()
+const reload = browserSync.reload
 
-function tarefasCSS(cb) {
+function tarefasCSS(callback) {
 
-    return gulp.src([
+    gulp.src([
             './node_modules/bootstrap/dist/css/bootstrap.css',
             './node_modules/@fortawesome/fontawesome-free/css/fontawesome.css',
             './vendor/owl/css/owl.css',
@@ -19,13 +24,15 @@ function tarefasCSS(cb) {
         .pipe(stripCss())               
         .pipe(concat('styles.css'))         
         .pipe(rename({ suffix: '.min'}))    
-        .pipe(gulp.dest('./dist/css'))     
+        .pipe(gulp.dest('./dist/css'))  
+        
+    return callback()
 
 }
 
-function tarefasJS(){
+function tarefasJS(callback){
 
-    return gulp.src([
+    gulp.src([
             './node_modules/jquery/dist/jquery.js',
             './node_modules/bootstrap/dist/js/bootstrap.js',
             './vendor/owl/js/owl.js',
@@ -33,11 +40,16 @@ function tarefasJS(){
             './vendor/jquery-ui/jquery-ui.js',
             './src/js/custom.js'
         ])
-        .pipe(stripJs())                    
+        .pipe(babel({
+            comments: false,
+            presets: ['@babel/env']
+        }))
         .pipe(concat('scripts.js'))         
         .pipe(uglify())                    
         .pipe(rename({ suffix: '.min'}))   
-        .pipe(gulp.dest('./dist/js'))      
+        .pipe(gulp.dest('./dist/js'))
+        
+    return callback()
 }
 
 
@@ -58,6 +70,29 @@ function tarefasImagem(){
         .pipe(gulp.dest('./dist/images'))
 }
 
+function tarefasHTML(callback){
+    gulp.src('./src/**/*.html')
+        .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(gulp.dest('./dist'))
+
+    return callback()    
+}
+
+gulp.task('serve', function(){
+
+    browserSync.init({
+        server: {
+            baseDir: "./dist"
+        }
+    })
+    gulp.watch('./src/**/*').on('change', process)
+    gulp.watch('./src/**/*').on('change', reload)
+
+})
+
+const process = series( tarefasHTML, tarefasJS, tarefasCSS )
+
 exports.styles = tarefasCSS
 exports.scripts = tarefasJS
 exports.images = tarefasImagem
+exports.default = process
